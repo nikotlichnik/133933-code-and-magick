a4476618d76230ecef577ca13cdcfa8b4be1c82b
nikotlichnik <shtreisnikita@gmail.com>
2018-06-08 11:55:04 +0300

    Из функций вынесено создание некоторых переменных, создана функция initPage

--- js/setup.js
+++ js/setup.js
@@ -7,18 +7,12 @@ var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Валь
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-
-// Функция, возвращающая случайное число от нуля до переданного параметра включительно
-var getRandomNumberInRange = function (endBorder) {
-  return Math.round(Math.random() * endBorder);
-};
-
 // Функция, возвращающая случайно перемешанную копию массива (тасование Фишера — Йетса)
 var getShuffledArray = function (array) {
   var shuffledArray = array.slice(0);
 
   for (var i = array.length - 1; i > 0; i--) {
-    var swapIndex = getRandomNumberInRange(i);
+    var swapIndex = Math.round(Math.random() * i);
     var swap = shuffledArray[swapIndex];
     shuffledArray[swapIndex] = shuffledArray[i];
     shuffledArray[i] = swap;
@@ -63,8 +57,6 @@ var generateSimilarWizards = function () {
 
 // Функция создания DOM-элемента волшебника на основе объекта с данными
 var createWizardElement = function (wizardData) {
-  var template = document.querySelector('#similar-wizard-template');
-  var similarWizardTemplate = template.content.querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizardData.name;
@@ -86,21 +78,26 @@ var createWizardsFragment = function () {
   return fragment;
 };
 
+// Фукнция инициализации страницы
+var initPage = function () {
+  // Показываем окно настройки
+  userDialog.classList.remove('hidden');
+
+  // Добавляем фрагмент с похожими волшебниками в нужный блок
+  userDialog.querySelector('.setup-similar-list').appendChild(createWizardsFragment());
+
+  // Показываем блок с похожими волшебниками
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+};
+
 // Генерация перетасованных массивов
 var shuffledNames = getShuffledArray(NAMES);
 var shuffledSurnames = getShuffledArray(SURNAMES);
 var shuffledCoatColors = getShuffledArray(COAT_COLORS);
 var shuffledEyesColors = getShuffledArray(EYES_COLORS);
 
-// Показываем окно настройки
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var template = document.querySelector('#similar-wizard-template');
+var similarWizardTemplate = template.content.querySelector('.setup-similar-item');
 
-// Создаём фрагмент, содержащий похожих волшебников
-var similarWizardsFragment = createWizardsFragment();
-
-// Добавляем фрагмент с похожими волшебниками в нужный блок
-userDialog.querySelector('.setup-similar-list').appendChild(similarWizardsFragment);
-
-// Показываем блок с похожими волшебниками
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+initPage();
